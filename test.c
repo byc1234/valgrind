aaeeca631a3d47cd5ab9949590ee0df691e33832
Hwan Lee <hwansoo.lee@gmail.com>
2017-02-11 18:42:30 -0500

    .

--- test.c
+++ test.c
@@ -1,10 +1,14 @@
   #include <stdlib.h>
-  #include <unistd.h>
-  int main( void )
+
+  void f(void)
   {
-    char* arr  = malloc(10);
-    int*  arr2 = malloc(sizeof(int));
-    write( 1 /* stdout */, arr, 10 );
-    exit(arr2[0]);
+     int* x = malloc(10 * sizeof(int));
+     x[10] = 0;        // problem 1: heap block overrun
+  }                    // problem 2: memory leak -- x not freed
+
+  int main(void)
+  {
+     f();
+     return 0;
   }
 
